f05f31d1ba0c88b07279c4b1e44d317876ee97e9
辻昌紘 <tsujiakihiro@tsujimayuminoMacBook-puro.local>
2019-01-09 13:38:01 +0900

    １−２〜２−１０

--- Practice/src/practice01/PTra01_06.java
+++ Practice/src/practice01/PTra01_06.java
@@ -1,4 +1,5 @@
 package practice01;
+
 /*
  * PTra01_06.java
  *   作成	LIKEIT	2017
@@ -10,13 +11,13 @@ public class PTra01_06 {
 	public static void main(String[] args) {
 
 		// ★ 文字列を格納する変数 str を宣言してください
-
+		String str;
 
 		// ★ 変数 str に自分の名前を代入してください
-
+		str = "辻昌紘";
 
 		// ★ 変数 str の中身を出力してください
-
+		System.out.println(str);
 
 	}
 }
--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,24 +21,24 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
-
-
-
-
-
-
+		bo = true;
+		by = 1;
+		s = 1;
+		c = 'O';
+		i = 1;
+		l = 1L;
+		f = 1.0F;
+		d = 1.0;
 
 		// ★ それぞれの変数の中身を出力してください
-
-
-
-
-
-
-
-
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 	}
 }
