bf78cea56adfc8e352e4ff1d3bdd5031ad8ed6a8
TsuziAkihiro <akihiro.tsuzi.likeit@gmail.com>
2019-01-10 16:57:13 +0900

    7-1 ~ 7-6

--- Practice/src/practice07/PTra07_06.java
+++ Practice/src/practice07/PTra07_06.java
@@ -16,6 +16,12 @@ public class PTra07_06 {
 		};
 
 		// ★ 配列arrayに格納されている全ての数字の平均を出力してください
-
+		int sum = 0;
+		for(int i =0;i < array.length;i++) {
+			for(int j = 0; j < array[i].length;j++) {
+				sum += array[i][j];
+			}
+		}
+		System.out.println(sum);
 	}
 }
